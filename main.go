8021656441c907f6e33aeb842b01fa9f8caa13f4
NickMcGrath <nickmcgrath@live.ca>
2021-11-14 17:48:32 -0800

    Created beta of backend chat room

--- main.go
+++ main.go
@@ -1,3 +1,139 @@
-package chatroombackend
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
 
+var rooms = make(map[string]*Room)
+
+type ChatMessage struct {
+	User string
+	Text string
+}
+type User struct {
+	Conn *websocket.Conn
+	Name string
+}
+
+func (user *User) SendMessage(chatMessage *ChatMessage) error {
+	chatMessageBytes, err := json.Marshal(chatMessage)
+	if err != nil {
+		log.Println(err)
+		//json parse error not connection error, forgetttta bout et
+		return nil
+	}
+	log.Println(user.Name, "Sending message:", *chatMessage)
+	if err := user.Conn.WriteJSON(string(chatMessageBytes)); err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+func (user *User) ReceiveMessage() (ChatMessage, error) {
+	cm := ChatMessage{}
+	//add proper error handing
+	if err := user.Conn.ReadJSON(&cm); err != nil {
+		log.Println(user.Name, "has error:", err)
+		return cm, err
+	}
+	log.Println(user.Name, "Received message:", cm)
+	return cm, nil
+}
+
+type Room struct {
+	Channel chan ChatMessage
+	Users   []*User
+}
+
+func (room *Room) Broadcast() {
+	go func() {
+		for {
+			msg := <-room.Channel
+			log.Println("Taking off channel")
+			for _, user := range room.Users {
+				log.Println("sending to", user)
+				if err := user.SendMessage(&msg); err != nil {
+					room.RemoveUser(user)
+				}
+			}
+		}
+	}()
+}
+func (room *Room) AddUser(user *User) {
+	room.Users = append(room.Users, user)
+	go func() {
+		for {
+			cm, err := user.ReceiveMessage()
+			if err != nil {
+				log.Println("Receive message issue, removing user", user.Name)
+				room.RemoveUser(user)
+				break
+			}
+			room.Channel <- cm
+			log.Println("Writing to channel")
+		}
+	}()
+}
+func (room *Room) RemoveUser(user *User) {
+	for i, userVal := range room.Users {
+		if userVal == user {
+			room.Users = append(room.Users[:i], room.Users[i+1:]...)
+			break
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home page")
+}
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	roomParam := r.URL.Query().Get("room")
+	nameParam := r.URL.Query().Get("name")
+	log.Println("New Connection request: room:", roomParam, "name", nameParam)
+	if len(roomParam) == 0 || len(nameParam) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Invalid Request Parameters")
+		return
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	user := User{
+		Name: nameParam,
+		Conn: ws,
+	}
+	if room, ok := rooms[roomParam]; ok {
+		room.AddUser(&user)
+	} else {
+		newRoom := Room{Channel: make(chan ChatMessage)}
+		newRoom.AddUser(&user)
+		newRoom.Broadcast()
+		rooms[roomParam] = &newRoom
+	}
+	log.Println("Client Successfully Connected...")
+	log.Println(rooms)
+}
+
+func setUpRoutes() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Go WebSockets")
+	setUpRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
